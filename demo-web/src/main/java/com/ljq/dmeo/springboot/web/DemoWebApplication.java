a494288d658812ea073df042fcd61c27160378da
junqiang.lu <flying9001@gmail.com>
2018-09-13 11:59:51 +0800

    使用springBoot 创建maven web 多模块项目

--- demo-web/src/main/java/com/ljq/dmeo/springboot/web/DemoWebApplication.java
+++ demo-web/src/main/java/com/ljq/dmeo/springboot/web/DemoWebApplication.java
@@ -1,12 +1,12 @@
-package com.ljq.dmeo.springboot;
+package com.ljq.dmeo.springboot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class DemoWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
+        SpringApplication.run(DemoWebApplication.class, args);
     }
 }
--- demo-web/src/test/java/com/ljq/dmeo/springboot/web/DemoWebApplicationTests.java
+++ demo-web/src/test/java/com/ljq/dmeo/springboot/web/DemoWebApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ljq.dmeo.springboot;
+package com.ljq.dmeo.springboot.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootApplicationTests {
+public class DemoWebApplicationTests {
 
     @Test
     public void contextLoads() {
