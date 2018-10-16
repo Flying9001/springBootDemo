a8f41256dae64ec23be85d72138c917be984da24
junqiang.lu <flying9001@gmail.com>
2018-10-16 23:13:13 +0800

    添加springCloud模块

--- demo-web/src/main/java/com/ljq/demo/springboot/web/DemoWebApplication.java
+++ demo-web/src/main/java/com/ljq/demo/springboot/web/DemoWebApplication.java
@@ -4,9 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaServer
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.ljq.demo.springboot"})
 @MapperScan("com.ljq.demo.springboot.dao")
