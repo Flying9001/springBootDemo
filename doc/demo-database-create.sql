81fa4bc8e5d36c4f7fc4deac320fe1deb3001b7f
junqiang.lu <flying9001@gmail.com>
2018-10-22 17:59:43 +0800

    新增 spring boot 多环境配置,修改时间格式为string(便于后期DB维护)

--- doc/demo-database-create.sql
+++ doc/demo-database-create.sql
@@ -15,9 +15,9 @@ create table user
    user_name            varchar(30) comment '用户名',
    user_passcode        varchar(100) comment '登陆密码',
    user_email           varchar(50) comment '邮箱',
-   user_insert_time     datetime comment '用户注册时间',
-   user_update_time     datetime comment '用户更新时间',
-   user_status          tinyint comment '用户账号状态,1正常(默认),2禁止登陆',
+   user_insert_time     varchar(30) comment '用户注册时间',
+   user_update_time     varchar(30) comment '用户更新时间',
+   user_status          tinyint default 1 comment '用户账号状态,1正常(默认),2禁止登陆',
    user_version         int unsigned default 1 comment '版本控制字段(默认1)',
    user_del             tinyint default 0 comment '逻辑删除字段,0正常(默认),1删除',
    primary key (id)
