04b413e6a30b45c994f5d698bf0a55b92af066bb
junqiang.lu <flying9001@gmail.com>
2018-10-26 10:57:13 +0800

    解决 spring boot 通过 war 包部署到外部 tomcat 抛出 404 异常的问题

--- demo-web/src/main/java/com/ljq/demo/springboot/web/DemoWebApplication.java
+++ demo-web/src/main/java/com/ljq/demo/springboot/web/DemoWebApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -12,9 +15,15 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.ljq.demo.springboot"})
 @MapperScan("com.ljq.demo.springboot.dao")
-public class DemoWebApplication {
+@ServletComponentScan
+public class DemoWebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoWebApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoWebApplication.class);
     }
 }
