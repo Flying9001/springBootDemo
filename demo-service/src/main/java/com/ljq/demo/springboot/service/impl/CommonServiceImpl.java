99a80f83076ccf535814e6088ac8cbbf41efa62e
junqiang.lu <flying9001@gmail.com>
2018-12-25 13:31:58 +0800

    springBoot下载功能添加服务器本地文件下载

--- demo-service/src/main/java/com/ljq/demo/springboot/service/impl/CommonServiceImpl.java
+++ demo-service/src/main/java/com/ljq/demo/springboot/service/impl/CommonServiceImpl.java
@@ -28,7 +28,7 @@ public class CommonServiceImpl implements CommonService {
         HttpHeaders headers = new HttpHeaders();
 
         /**
-         * 文件名为 null,返回 404 页面
+         * 文件名为 null,返回字符串
          */
         if (downloadBean.getFileName() == null || "".equalsIgnoreCase(downloadBean.getFileName())) {
             headers.setContentType(MediaType.TEXT_HTML);
@@ -36,31 +36,49 @@ public class CommonServiceImpl implements CommonService {
         }
 
         /**
-         * 文件名不是 "demo",返回 json 格式字符串
+         * 根据文件名下载对应的文件
+         * fileName:
+         *     url: 下载网络文件
+         *     resource: 下载项目中的文件
+         *     local: 下载(服务器)本地文件
          */
-        if (!"demo".equalsIgnoreCase(downloadBean.getFileName())) {
-            headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-            return new ResponseEntity<String>("{ \"code\" : \"404\", \"message\" : \"not found\" }",
-                    headers, HttpStatus.NOT_FOUND);
+        switch (downloadBean.getFileName()) {
+            case "url":
+                downloadBean.setFileName("https://upload-images.jianshu.io/upload_images/4412479-6895144ef53d208c.jpg?" +
+                        "imageMogr2/auto-orient/strip%7CimageView2/2/w/1240");
+                byte[] fileBytes = FileUtil.getBytesFromURLFile(downloadBean.getFileName());
+                if (fileBytes != null && fileBytes.length > 0) {
+                    headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+                    headers.setContentDispositionFormData("attachment", "DEMO" + System.currentTimeMillis() + ".jpg");
+                    return new ResponseEntity<byte[]>(fileBytes, headers, HttpStatus.OK);
+                }
+                break;
+            case "resource":
+                downloadBean.setFileName("static/README.pdf");
+                byte[] resourceBytes = FileUtil.getBytesFromResource(downloadBean.getFileName());
+                if (resourceBytes != null && resourceBytes.length > 0) {
+                    headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+                    headers.setContentDispositionFormData("attachment", "DEMO" + System.currentTimeMillis() + ".pdf");
+                    return new ResponseEntity<byte[]>(resourceBytes, headers, HttpStatus.OK);
+                }
+                break;
+            case "local" :
+                downloadBean.setFileName("E:\\software\\tools\\AdbeRdr11_zh_CN11.exe");
+                byte[] fileByteArray = FileUtil.getByteFromFileWithNIO(downloadBean.getFileName());
+                if (fileByteArray != null &&fileByteArray.length > 0) {
+                    headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+                    headers.setContentDispositionFormData("attachment", "DEMO" + System.currentTimeMillis() + ".exe");
+                    return new ResponseEntity<byte[]>(fileByteArray, headers, HttpStatus.OK);
+                }
+                break;
+            default:
+                headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+                return new ResponseEntity<String>("{ \"code\" : \"404\", \"message\" : \"not found\" }",
+                        headers, HttpStatus.NOT_FOUND);
         }
 
-        /**
-         * 文件名为 "demo",返回文件
-         */
-        if ("demo".equalsIgnoreCase(downloadBean.getFileName())) {
-            downloadBean.setFileName("https://upload-images.jianshu.io/upload_images/4412479-6895144ef53d208c.jpg?" +
-                    "imageMogr2/auto-orient/strip%7CimageView2/2/w/1240");
-        }
-
-        byte[] fileBytes = FileUtil.toBytesFromURLFile(downloadBean.getFileName());
-
-        if (fileBytes == null || fileBytes.length == 0) {
-            headers.setContentType(MediaType.TEXT_HTML);
-            return new ResponseEntity<String>("File doesn't exist", headers, HttpStatus.NOT_FOUND);
-        }
-
-        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-        headers.setContentDispositionFormData("attachment", "DEMO" + System.currentTimeMillis() + ".jpg");
-        return new ResponseEntity<byte[]>(fileBytes, headers, HttpStatus.OK);
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        return new ResponseEntity<String>("{ \"code\" : \"404\", \"message\" : \"not found\" }",
+                headers, HttpStatus.NOT_FOUND);
     }
 }
